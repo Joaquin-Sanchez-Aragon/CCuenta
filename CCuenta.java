c86e4d95c0eba72d55a022184e602174240c3d0f
Joaquin-Sanchez-Aragon <124196753+Joaquin-Sanchez-Aragon@users.noreply.github.com>
2023-02-02 18:05:33 +0100

    Add files via upload

---
+++ CCuenta.java
@@ -0,0 +1,50 @@
+package cuentas;
+
+class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        setSaldo(sal);
+    }
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
